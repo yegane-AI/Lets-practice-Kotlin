09657523637b817340c3d5ee291594cdda011650
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-10 02:25:40 +0430

    Add files via upload
    "if" and "when" :)

--- second.kt
+++ second.kt
@@ -1,8 +1,13 @@
 var name = "Yegane"
 var salutation: String? = "Salam jigar!"
 
-fun main(){ 
+fun main(){
     var saluteTo_me = if (salutation != null) salutation else "Hello jigar"
     println(saluteTo_me)
     println(name)
+
+    when(salutation){
+        null -> println("It is null")
+        else -> println(salutation)
+    }
 }
