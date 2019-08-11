6a1727ee8a35dc0160e1d0b2539de5c6d2712865
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-12 01:30:45 +0430

    Update concat_str.kt

--- concat_str.kt
+++ concat_str.kt
@@ -2,5 +2,5 @@ var salam = "Hi Miss " /* My first string */
 var esm = "Yegane" /* My second string */
 
 fun main(){
-    println(salam+esm) /* Concat the two strings.  */
+    println(salam+esm+". "+"It's a concat!!! ") /* Concat the two strings.  */
 }
