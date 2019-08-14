5ecaeed1cd9fc4d814f173e2980a9440c0ebed62
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-14 20:56:18 +0430

    Update boolean.kt

--- boolean.kt
+++ boolean.kt
@@ -1,4 +1,6 @@
-var bool = 10 > 11
+var bool1 = 10 > 11
+var bool2 = 10 < 11
 fun main(){
-  println(bool)
+  println(bool1) /* While 10 is not greater that 11, the program returns false. */ 
+  println(bool2) /* While 10 is smaller that 11, the program returns true. */
 }
